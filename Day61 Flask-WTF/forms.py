e158217e8458cb0dc2420a1b983d6adff64f15a8
doh6077 <kimdohee@sheridancollege.ca>
2024-04-28 18:57:28 -0400

    Create HTML login form template with email and password fields

--- Day61 Flask-WTF/forms.py
+++ Day61 Flask-WTF/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 
 class MyForm(FlaskForm):
     email = StringField('email', validators=[DataRequired()])
-    password = StringField('password', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
+    submit =SubmitField("Log In")
