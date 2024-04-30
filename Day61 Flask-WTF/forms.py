626e79c44b7177eb4c17d9658e9e0ce5ea26e9e9
82108 <doh6077@gmail.com>
2024-04-30 09:10:35 -0400

    Add validator for email form

--- Day61 Flask-WTF/forms.py
+++ Day61 Flask-WTF/forms.py
@@ -3,6 +3,6 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
 
 class MyForm(FlaskForm):
-    email = StringField('email', validators=[Email(message = "Invalid email address")])
+    email = StringField('email', validators=[DataRequired(), Email(message = "Invalid email address")])
     password = PasswordField('password', validators=[DataRequired(), Length(min=7)])
     submit =SubmitField("Log In")
