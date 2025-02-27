04d4fa11d9566fb9379f992e89ac719f754282fb
doh6077 <kimdohee@sheridancollege.ca>
2025-02-27 17:06:54 -0500

    Complete task

--- day-66-Review/main.py
+++ day-66-Review/main.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Boolean
-
+import random 
 '''
 Install the required packages first: 
 Open the Terminal in PyCharm (bottom left). 
@@ -52,7 +52,46 @@ def home():
 
 
 # HTTP GET - Read Record
+# in Python get is default 
+"""
+@app.route("/random")
+def get_random_cafe():
+    all_cafes = db.session.query(Cafe).all()
+    print("This is all cafe", all_cafes)
+    cafe = random.choice(all_cafes)
+    return_cafe = [{
+        "can_take_calls": cafe.can_take_calls,
+        "coffee_price" : cafe.coffee_price,
+        "has_sockets" : cafe.has_sockets,
+        "has_toilet" : cafe.has_toilet,
+        "has_wifi" : cafe.has_wifi,
+        "id" : cafe.id,
+        "img_url" : cafe.img_url,
+        "Location" : cafe.location,
+        "map_url" : cafe.map_url,
+        "name" : cafe.name,
+        "seats" : cafe.seats
+    }]
 
+    return jsonify(return_cafe) 
+"""
+@app.route("/random")
+def get_random_cafe():
+    cafes = db.session.query(Cafe).all()
+    random_cafe = random.choice(cafes)
+    return jsonify(cafe={
+        "id": random_cafe.id,
+        "name": random_cafe.name,
+        "map_url": random_cafe.map_url,
+        "img_url": random_cafe.img_url,
+        "location": random_cafe.location,
+        "seats": random_cafe.seats,
+        "has_toilet": random_cafe.has_toilet,
+        "has_wifi": random_cafe.has_wifi,
+        "has_sockets": random_cafe.has_sockets,
+        "can_take_calls": random_cafe.can_take_calls,
+        "coffee_price": random_cafe.coffee_price,
+    })
 # HTTP POST - Create Record
 
 # HTTP PUT/PATCH - Update Record
