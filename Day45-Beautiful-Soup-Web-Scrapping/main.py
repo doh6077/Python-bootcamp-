bf32a59b1ee85e6c35a6eedcb857c8105ea0b731
doh6077 <kimdohee@sheridancollege.ca>
2025-05-14 16:14:38 -0400

    Complete step 1

--- Day45-Beautiful-Soup-Web-Scrapping/main.py
+++ Day45-Beautiful-Soup-Web-Scrapping/main.py
@@ -1,16 +1,24 @@
 from bs4 import BeautifulSoup
+import requests
+# with open("website.html", encoding="utf-8") as file:
+#     contents = file.read()
 
-with open("website.html", encoding="utf-8") as file:
-    contents = file.read()
+# soup = BeautifulSoup(contents, 'html.parser')
 
-soup = BeautifulSoup(contents, 'html.parser')
+# print(soup.prettify())
 
+
+# lists = soup.find_all('li')
+# list_text = []
+# for list in lists:
+#     list_text.append(list.get_text())
+
+# soup.select_one()
+
+response = requests.get('https://news.ycombinator.com/news')
+
+web_page = response.text
+
+
+soup = BeautifulSoup(web_page, 'html.parser')
 print(soup.prettify())
-
-
-lists = soup.find_all('li')
-list_text = []
-for list in lists:
-    list_text.append(list.get_text())
-
-soup.select_one()
