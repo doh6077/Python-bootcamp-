ab6fe641dd01e6ab2746e9c6459166ece02aeca7
doh6077 <kimdohee@sheridancollege.ca>
2025-05-15 14:30:59 -0400

    Optimze the code

--- Day93-Potofolio-Web-Scrapping/main.py
+++ Day93-Potofolio-Web-Scrapping/main.py
@@ -8,6 +8,5 @@ load_dotenv()  # take environment variables from .env
 response = requests.get("https://www.audible.com/search?keywords=book&node=18573211011&overrideBaseCountry=true&ipRedirectOverride=true&ref_pageloadid=not_applicable&pf_rd_p=97a1c91a-2694-43e4-ad66-256f5ac11479&pf_rd_r=F9RMKBR8PMAM4VM3XHWC&plink=2C9gPIFJK0X9yOoZ&pageLoadId=nLVcNR8KvLZVf2MN&creativeId=292d6343-f11b-4bbe-a8a5-d4b7272abf61")
 soup = BeautifulSoup(response.text, 'html.parser')
 books = soup.find_all(class_="bc-link bc-color-link")
-book_names = [book.getText().strip() for book in books ]
-book_names = [ book for book in book_names if len(book) > 1]
+book_names = [book.getText().strip() for book in books if len(book.getText().strip()) > 1]
 print(book_names)
