3ec4cd16778e0adcba6ecd5d4db03a2fc57de439
doh6077 <kimdohee@sheridancollege.ca>
2025-05-15 13:57:07 -0400

    Complete the step 4

--- Day46-Musical-Time-Machine-Project/main.py
+++ Day46-Musical-Time-Machine-Project/main.py
@@ -29,7 +29,7 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
 }
 date = '2000-08-12'
-date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+# date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
 
 
 response = requests.get(f'{HTML_DOC}/{date}', headers=headers)
@@ -40,25 +40,19 @@ song_names_spans = soup.select("li ul li h3")
 song_names = [song.getText().strip() for song in song_names_spans]
 spotify_songs = [] 
 urls = []
-print(date.split('-')[0])
 for song in song_names:
-    result = sp.search(q='track:'+ song +'year:'+ date.split('-')[0], type="track")
-    pprint.pprint(result['tracks']['href'])
-    urls.append(result)
-
-# print(urls)
-# 1. 스포티파이 문서를 보고, 1단계(빌보드 탑 100 스크래핑)에서 찾은 곡 목록에 해당하는 스포티파이 노래 URI 목록을 만드세요.
-
-# 힌트 1: 특정 연도에 나온 곡명으로 좁히기 위해 쿼리 형식은 ‘track: {name} year: {YYYY}’을 사용합니다.
-
-# 힌트 2: 스포티파이에 곡이 없을 수도 있으니, 예외 처리를 통해 해당 곡들을 건너뛰세요.
-
-# 힌트 3: pprint()를 사용해 결과를 보기 좋게 출력하세요. https://docs.python.org/3/library/pprint.html
+    result = sp.search(q='track:' + song + ' year:' + date.split('-')[0], type="track")
+    try:
+        track_uri = result['tracks']['items'][0]['uri']
+        urls.append(track_uri)
+    except (IndexError, KeyError):
+        print(f"Track not found for: {song}")
 
 
-# tracks(tracks, market=None)
-# returns a list of tracks given a list of track IDs, URIs, or URLs
+user_id = sp.current_user()["id"]
+playlists = sp.user_playlists(user_id)
+playlist = sp.user_playlist_create(user=user_id, name=f"{date} Billboard 100", public=False)
 
-# Parameters:
-# tracks - a list of spotify URIs, URLs or IDs. Maximum: 50 IDs.
-# market - an ISO 3166-1 alpha-2 country code.
+# Add songs from step3 to the playlist 
+
+sp.user_playlist_add_tracks(user=user_id, playlist_id=playlist['id'], tracks=urls)
