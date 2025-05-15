a7e671d2a2a2e880c62bc6bbe3b464ca795194f9
doh6077 <kimdohee@sheridancollege.ca>
2025-05-14 21:19:59 -0400

    Refactor: Clean up Spotify authentication and retrieve user info
    - Removed unnecessary comments from Spotify authentication section.
    - Successfully implemented Spotipy API authentication using environment variables.
    - Retrieved current Spotify user information after authentication.

--- Day46-Musical-Time-Machine-Project/main.py
+++ Day46-Musical-Time-Machine-Project/main.py
@@ -1,5 +1,26 @@
 from bs4 import BeautifulSoup
 import requests
+# filepath: main.py
+from dotenv import load_dotenv
+import os
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+
+load_dotenv()  # take environment variables from .env
+
+auth = spotipy.SpotifyOAuth(client_id=os.getenv("CLIENT_ID"),
+                    client_secret=os.getenv("CLIENT_SECRET"),
+                    redirect_uri="http://example.com",
+                    scope="playlist-modify-private")
+
+
+#auth.get_access_token()
+
+sp = spotipy.Spotify(auth = os.getenv("SPOTIPY_ACCESS_TOKEN"))
+user = sp.current_user()
+user_id = user.display_name
+
 
 HTML_DOC = "https://www.billboard.com/charts/hot-100/"
 
@@ -8,13 +29,13 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
 }
 
-date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+# date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
 
 
-response = requests.get(f'{HTML_DOC}/{date}', headers=headers)
-soup = BeautifulSoup(response.text, 'html.parser')
+# response = requests.get(f'{HTML_DOC}/{date}', headers=headers)
+# soup = BeautifulSoup(response.text, 'html.parser')
 
-song_names_spans = soup.select("li ul li h3")
-print(song_names_spans)
-song_names = [song.getText().strip() for song in song_names_spans]
-print(song_names)
+# song_names_spans = soup.select("li ul li h3")
+# print(song_names_spans)
+# song_names = [song.getText().strip() for song in song_names_spans]
+# print(song_names)
