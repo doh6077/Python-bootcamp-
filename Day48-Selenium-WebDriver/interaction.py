45a1d3bbf0cc60a95c5981e72f55bf0bb2af35f0
doh6077 <kimdohee@sheridancollege.ca>
2025-05-16 17:20:40 -0400

    Use search bar

--- Day48-Selenium-WebDriver/interaction.py
+++ Day48-Selenium-WebDriver/interaction.py
@@ -1,6 +1,9 @@
 from selenium import webdriver 
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.common.keys import Keys
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome()
@@ -8,4 +11,18 @@ driver.get("https://en.wikipedia.org/wiki/Main_Page")
 
 number = driver.find_element(By.XPATH, value='//*[@id="articlecount"]/ul/li[2]/a[1]')
 
-print(number.text)
+
+# Find by the link text 
+all_portals = driver.find_element(By.LINK_TEXT, "Help desk")
+# all_portals.click()
+
+
+# ...existing code...
+
+search = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.NAME, "search"))
+)
+search.send_keys("Python")
+search.send_keys(Keys.ENTER)
+
+time.sleep(0.1)
