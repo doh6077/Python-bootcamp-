50186fc3b09c83bec8509dfdab69a85e423fe9aa
doh6077 <kimdohee@sheridancollege.ca>
2025-05-20 10:52:53 -0400

    Complete step 2: automatic sign-in functionality

---
+++ Day49-Automated-Job-Application-with-Selenium/main.py
@@ -0,0 +1,41 @@
+from selenium import webdriver 
+from selenium.webdriver.common.by import By
+import time
+import os 
+from selenium.webdriver.common.keys import Keys
+from dotenv import load_dotenv
+load_dotenv()
+
+url ="https://www.linkedin.com/jobs/collections/easy-apply/?currentJobId=4220162457&discover=recommended&discoveryOrigin=JOBS_HOME_JYMBII"
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=chrome_options)
+driver.get(url)
+driver.implicitly_wait(time_to_wait=10) 
+
+# sign_in = driver.find_element(By.CLASS_NAME, 'nsm7Bb-HzV7m-LgbsSe-BPrWId')
+# sign_in.click()
+time.sleep(5)
+
+EMAIL = os.getenv("EMAIL")
+PASSWORD = os.getenv("PASSWORD")
+
+
+# input email and password 
+email = driver.find_element(By.ID,"username")
+password = driver.find_element(By.ID,"password")
+
+email.send_keys(EMAIL)
+# email.send_keys(Keys.ENTER)
+
+password.send_keys(PASSWORD)
+# password.send_keys(Keys.ENTER)
+
+# driver.quit()
+
+# find button for sign in 
+time.sleep(5)
+sign_in = driver.find_element(By.CLASS_NAME, 'from__button--floating')
+print(sign_in.text)
+sign_in.click()
+time.sleep(5)
